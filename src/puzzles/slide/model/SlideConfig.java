df1304fdefa1d9e3525178c4d3ee432830cabc33
Trent Wesley <taw8452@rit.edu>
2021-12-02 23:20:50 -0500

    Started implementing Slide and SlideConfig

--- src/puzzles/slide/model/SlideConfig.java
+++ src/puzzles/slide/model/SlideConfig.java
@@ -1,4 +1,90 @@
 package puzzles.slide.model;
 
-public class SlideConfig  {
+import puzzles.common.solver.Configuration;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * This class represents a slide puzzle configuration
+ *
+ * @author Trent Wesley taw8452
+ */
+public class SlideConfig implements Configuration {
+    private int rows;
+    private int columns;
+    private int[][] grid;
+
+    public SlideConfig(int rows, int columns, int[][] grid){
+        this.rows = rows;
+        this.columns = columns;
+        this.grid = grid;
+    }
+
+
+    @Override
+    public boolean isSolution() {
+        int previous = 0;
+        for (int r = 0; r < rows; r++){
+            for (int c = 0; c < columns; c++){
+                if (grid[r][c] != ++previous) return false;}}
+        return true;
+    }
+
+    @Override
+    public List<Configuration> getNeighbors() {
+        int row=0; int col=0;
+        for (int r = 0; r < rows; r++){
+            for (int c = 0; c < columns; c++){
+                if (grid[r][c] == rows*columns){
+                row = r;  col = c;}}}
+
+        List<Configuration> configs = new LinkedList<>();
+        if (row-1 >= 0){
+            int[][] newGrid = copyGrid(grid);
+            newGrid[row][col] = newGrid[row-1][col];
+            newGrid[row-1][col] = rows*columns;
+            Configuration c = new SlideConfig(rows, columns, newGrid);
+            configs.add(c);}
+        if (row+1 < rows){
+            int[][] newGrid = copyGrid(grid);
+            newGrid[row][col] = newGrid[row+1][col];
+            newGrid[row+1][col] = rows*columns;
+            Configuration c = new SlideConfig(rows, columns, newGrid);
+            configs.add(c);}
+        if (col-1 >= 0){
+            int[][] newGrid = copyGrid(grid);
+            newGrid[row][col] = newGrid[row][col-1];
+            newGrid[row][col-1] = rows*columns;
+            Configuration c = new SlideConfig(rows, columns, newGrid);
+            configs.add(c);}
+        if (col+1 < columns){
+            int[][] newGrid = copyGrid(grid);
+            newGrid[row][col] = newGrid[row][col+1];
+            newGrid[row][col+1] = rows*columns;
+            Configuration c = new SlideConfig(rows, columns, newGrid);
+            configs.add(c);}
+
+        return configs;
+    }
+
+    private int[][] copyGrid(int[][] grid){
+        int[][] copy = new int[grid.length][grid[0].length];
+        for (int r = 0; r < rows; r++){
+            for (int c = 0; c < columns; c++){
+                copy[r][c] = grid[r][c];}}
+        return copy;
+    }
+
+    @Override
+    public String toString(){
+        String string = "";
+        for (int r = 0; r < rows; r++){
+            for (int c = 0; c < columns; c++){
+                // TODO
+            }
+        }
+
+        return string;
+    }
 }
--- src/puzzles/slide/solver/Slide.java
+++ src/puzzles/slide/solver/Slide.java
@@ -1,11 +1,45 @@
 package puzzles.slide.solver;
 
+import puzzles.common.solver.Configuration;
 import puzzles.common.solver.Solver;
+import puzzles.slide.model.SlideConfig;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
 
 public class Slide {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         if (args.length != 1) {
             System.out.println("Usage: java Slide filename");
         }
+        Scanner fileScanner = new Scanner(new File(args[0]));
+        int rows = fileScanner.nextInt();
+        int columns = fileScanner.nextInt();
+        int[][] grid = new int[rows][columns];
+        for (int r = 0; r < rows; r++){
+            for (int c = 0; c < columns; c++){
+                String thing = fileScanner.next();
+                if (thing.equals(".")) grid[r][c] = rows*columns;
+                else grid[r][c] = Integer.parseInt(thing);}}
+
+        // create the initial SlideConfig
+        SlideConfig initSlide = new SlideConfig(rows, columns, grid);
+
+        // create Solver
+        Solver solver = new Solver();
+
+        // calls Solver's solve with the initial config and get solution path
+        List<Configuration> path = solver.solve(initSlide);
+        System.out.println("Total configs: " + solver.getTotalConfigs());
+        System.out.println("Unique configs: " + solver.getUniqueConfigs());
+
+        // print solution path if it exists
+        if (path == null) System.out.println("No solution");
+        else{
+            for (int i = 0; i < path.size(); i++){
+                System.out.println("Step " + i + ": ");
+            }
+        }}
     }
-}
